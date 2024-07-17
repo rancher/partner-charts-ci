5f85dfb2936ea5649ab0a8648bd2bff6c9bf8454
Adam Pickering <adam.pickering@suse.com>
2024-07-17 14:50:03 -0600

    Introduce paths.Paths

--- pkg/paths/paths.go
+++ pkg/paths/paths.go
@@ -8,9 +8,42 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-// GetRepoRoot fetches absolute repository root path. If the
-// working directory is not the root of a git repo, exits the
-// program with an error.
+var paths *Paths
+
+// Paths is a type that contains all of the paths that are relevant
+// to this tool. This approach facilitates unit testing.
+type Paths struct {
+	Assets            string
+	Charts            string
+	ConfigurationYaml string
+	Icons             string
+	IndexYaml         string
+	Packages          string
+	RepoRoot          string
+}
+
+func Get() Paths {
+	if paths == nil {
+		// TODO: once GetRepoRoot is no longer used, remove this call and inline
+		// necessary parts of the code. We call it here in order to make sure
+		// we are at the repository root.
+		repoRoot := GetRepoRoot()
+		assets := "assets"
+		paths = &Paths{
+			Assets:            assets,
+			Charts:            "charts",
+			ConfigurationYaml: "configuration.yaml",
+			Icons:             filepath.Join(assets, "icons"),
+			IndexYaml:         "index.yaml",
+			Packages:          "packages",
+			RepoRoot:          repoRoot,
+		}
+	}
+	return *paths
+}
+
+// GetRepoRoot fetches absolute repository root path. If the working directory
+// is not the root of a git repo, exits the program with an error.
 func GetRepoRoot() string {
 	repoRoot, err := os.Getwd()
 	if err != nil {
