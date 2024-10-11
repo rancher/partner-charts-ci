0d182896dd909609ee932268d85596446e8ea060
Adam Pickering <adam.pickering@suse.com>
2024-10-11 12:06:25 -0600

    Make paths.GetRepoRoot exit with an error if not at root of git directory

--- pkg/paths/paths.go
+++ pkg/paths/paths.go
@@ -1,17 +1,29 @@
 package paths
 
 import (
+	"errors"
 	"os"
+	"path/filepath"
 
 	"github.com/sirupsen/logrus"
 )
 
-// Fetches absolute repository root path
+// GetRepoRoot fetches absolute repository root path. If the
+// working directory is not the root of a git repo, exits the
+// program with an error.
 func GetRepoRoot() string {
 	repoRoot, err := os.Getwd()
 	if err != nil {
-		logrus.Fatal(err)
+		logrus.Fatalf("failed to get working directory: %s", err)
 	}
 
-	return repoRoot
+	gitDirPath := filepath.Join(repoRoot, ".git")
+	if fileInfo, err := os.Stat(gitDirPath); err == nil && fileInfo.IsDir() {
+		return repoRoot
+	} else if errors.Is(err, os.ErrNotExist) || !fileInfo.IsDir() {
+		logrus.Fatalf("must be at the root of a git repo")
+	}
+
+	logrus.Fatalf("failed to check whether working directory is root of git repo: %s", err)
+	return ""
 }
