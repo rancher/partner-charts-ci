1b53b6274157e864a321cc6e77b62bbf9a892532
Adam Pickering <adam.pickering@suse.com>
2024-10-18 11:39:38 -0600

    Add comment explaining the idea behind ValidationFunc

--- pkg/validate/validate.go
+++ pkg/validate/validate.go
@@ -1,5 +1,8 @@
 package validate
 
+// A ValidationFunc is a function that checks one specific thing about the
+// partner charts repository. If anything is wrong, it may return one or
+// more errors explaining what is wrong.
 type ValidationFunc func(configYaml ConfigurationYaml) []error
 
 func Run(configYaml ConfigurationYaml) []error {
